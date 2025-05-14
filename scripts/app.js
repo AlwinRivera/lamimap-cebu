7f823e9cc4e084b84e5a2030da433f256b996598
Alwin Rivera <aimrivera@gmail.com>
2025-05-15 00:23:28 +0800

    Upload app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,36 @@
+const { createApp } = Vue;
 
+createApp({
+  data() {
+    return {
+      karinderyas: JSON.parse(localStorage.getItem("karinderyas")) || [
+        {
+          name: "Nanay's Lutong Bahay",
+          location: "Mabolo, near the church",
+          price: "₱60–₱90",
+          map: "https://maps.google.com"
+        }
+      ],
+      newName: '',
+      newLocation: '',
+      newPrice: '',
+      newMap: ''
+    };
+  },
+  methods: {
+    addKarinderya() {
+      const newK = {
+        name: this.newName,
+        location: this.newLocation,
+        price: this.newPrice,
+        map: this.newMap
+      };
+      this.karinderyas.push(newK);
+      localStorage.setItem("karinderyas", JSON.stringify(this.karinderyas));
+      this.newName = '';
+      this.newLocation = '';
+      this.newPrice = '';
+      this.newMap = '';
+    }
+  }
+}).mount('#app');
